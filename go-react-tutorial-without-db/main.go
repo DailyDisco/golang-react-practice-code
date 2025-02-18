53f588497d84bcb0892f3cf20e14a105927b67f3
DailyDisco <actual.mensch0r@icloud.com>
2025-02-18 14:59:33 -0600

    build the with-db version

--- go-react-tutorial-without-db/main.go
+++ go-react-tutorial-without-db/main.go
@@ -1,129 +1,216 @@
-// Main package is the entry point for the application
+// package main is the entry point of the program, it is the root package
 package main
 
-// fmt is a package that contains functions for formatting input and output, as well as scanning input, and printing output
+// import the fmt package which is used to print to the console
 import (
+	// context is used to handle the context of the request
+	"context"
+
+	// fmt is used to print to the console
 	"fmt"
+
+	// log is used to print to the console
 	"log"
+
+	// os is used to get the environment variables
 	"os"
 
+	// fiber is a web framework for Go, similar to express.js in Node.js for the backend
 	"github.com/gofiber/fiber/v2"
+
+	// godotenv is used to load the environment variables from the .env file
 	"github.com/joho/godotenv"
+
+	// bson is a binary JSON format used for MongoDB, similar to JSON but more efficient
+	"go.mongodb.org/mongo-driver/bson"
+
+	// primitive is used to represent the primitive types in MongoDB
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	// mongo is used to connect to the MongoDB database
+	"go.mongodb.org/mongo-driver/mongo"
+
+	// mongo/options is used to connect to the MongoDB database
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Structs are used to create custom types in Go
+
+// The todo struct is used to represent a todo in the database
 type Todo struct {
-	// Format is: ID int `json:"id"` 
-	// This is a struct tag, it is used to specify the JSON key name for the field
-	// ID is the value of the field, and int is the type of the field, and json is the key name in the JSON
-	// So the format is: field_name type `json:"key_name"`
-	ID int `json:"id"`
+	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
 
-func main(){
-	// Println is a function that prints a string to the console
-	fmt.Println("Hello, World!!")
+var collection *mongo.Collection
 
-	// App is a function that creates a new Fiber app, which is a web framework for Go that is used to create APIs
-	// Fiber is similar to Express.js in Node.js
-	app := fiber.New()
+// main function is the entry point of the program
+func main() {
 
+	// fmt.Println is used to print to the console
+	fmt.Println("Hello, World!")
+
+	// load the environment variables
 	err := godotenv.Load(".env")
+
+	// if the environment variables are not loaded, print an error
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	PORT := os.Getenv("PORT")
+	// get the port from the environment variables
+	MONGODB_URI := os.Getenv("MONGODB_URI")
 
-	todos := []Todo{}
+	// create the client options which are used to connect to the database
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
-	})
+	// connect to the database using the client options
+	// context.Background() is used to create a new context that is not cancelled for the duration of the connection
+	// think of it as a container that holds the connection to the database
+	client, err := mongo.Connect(context.Background(), clientOptions)
 
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
+	// if there is an error, print the error
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Create a new todo
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		// & is used to create a pointer to the todo struct
-		// A pointer is a variable that stores the memory address of another variable
-		// So todo is a pointer to a Todo struct
-		// &Todo{} is a pointer to a Todo struct
-		todo := &Todo{} // {id: 0, completed: false, body: ""}
+	// When the main function is finished, disconnect from the database
+	defer client.Disconnect(context.Background())
 
-		// BodyParser is a function that parses the body of the request and unmarshals it into the todo struct
-		if err := c.BodyParser(todo); err != nil {
+	// check if the connection to the database is successful
+	err = client.Ping(context.Background(), nil)
+
+	// if there is an error, print the error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// print a message to the console if the connection to the database is successful
+	fmt.Println("Connected to MongoDB ATLAS!")
+
+	// get the collection from the database
+	collection = client.Database("golang_db").Collection("todos")
+	
+	// create a new fiber app
+	app := fiber.New()
+
+	// get the todos from the collection
+	app.Get("/api/todos", getTodos)
+
+	// create a new todo
+	app.Post("/api/todos", createTodo)
+
+	// update a todo
+	app.Patch("/api/todos/:id", updateTodo)
+
+	// delete a todo
+	app.Delete("/api/todos/:id", deleteTodo)
+
+	// get the port from the environment variables
+	port := os.Getenv("PORT")
+
+	// if the port is not set, set it to 5000
+	if port == "" {
+		port = "5000"
+	}
+
+	// listen to the port
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
+
+// getTodos returns all the todos in the collection
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+
+	// find all the todos in the collection, the bson.M{} is the filter, which is empty in this case
+	// the end result is a cursor, which is used to iterate over the todos
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	// defer is used to close the cursor after the function is finished
+	defer cursor.Close(context.Background())
+
+	// iterate over the todos and decode them into the Todo struct
+	for cursor.Next(context.Background()) {
+		// decode the todo into the Todo struct
+		var todo Todo
+
+		// if there is an error, return the error	
+		if err := cursor.Decode(&todo); err != nil {
 			return err
 		}
 
-		// If the todo body is empty, return a 400 status code and a message
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
-		}
+		todos = append(todos, todo)
+	}
+	return c.JSON(todos)
+}
 
-		// Set the ID of the todo
-		// len(todos) is the length of the todos array
-		// +1 is the next number in the sequence
-		todo.ID = len(todos) + 1
+// createTodo creates a new todo in the collection
+func createTodo(c *fiber.Ctx) error {
+	todo := new(Todo)
 
-		// Append the todo to the todos array
-		todos = append(todos, *todo)
+	if err := c.BodyParser(&todo); err != nil {
+		return err
+	}
 
-		// Pointers explained:
-		// var x int =  5 // 0x14000112000 (memory address)
-		// var pointer *int = &x // 0x14000112000 (memory address is the same as x)
-		// fmt.Println(pointer) // 0x14000112000
-		// fmt.Println(*pointer) // 5 (* is used to get the value of the variable at the memory address)
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+	}
 
-		// Return a 201 status code and the todo
-		return c.Status(201).JSON(todo)
+	insertResult, err := collection.InsertOne(context.Background(), todo)
 
-	})
+	if err != nil {
+		return err
+	}
 
-	// Update a todo
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
 
-		// Go For Loop:
-		// Note: Go does not have a while loop, it only has a for loop
-		for i, todo := range todos {
-			// fmt.Sprint(todo.ID) is used to convert the ID to a string because id is a string
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
+	return c.Status(201).JSON(todo)
+}
 
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
+// updateTodo updates a todo in the collection
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
 
-	// Delete a todo
-	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-		for i, todo := range todos {
-			// fmt.Sprint(todo.ID) is used to convert the ID to a string because id is a string
-			if fmt.Sprint(todo.ID) == id {
-				// append(todos[:i], todos[i+1:]...) is used to remove the todo from the todos array
-				// 1 2 3 4 5
-				// Say we want to remove 3
-				// append(todos[:i], todos[i+1:]...) will remove 3 because it takes the elements before index i and the elements after index i+1
-				// So it will be 1 2 4 5
-				// This is a way to remove an element from an array in Go
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"message": "Todo deleted"})
-			}
-		}
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	}
 
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
 
-	// Listen is a function that listens for incoming requests on the specified port
-	// log.Fatal is a function that logs an error and then terminates the program
-	// ":4000" is the port number that the server will listen on
-	// The app is listening for incoming requests on port 4000, and has logged the error if it occurs
-	log.Fatal(app.Listen(":" + PORT))
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"message": "Todo updated"})
+}
+
+// deleteTodo deletes a todo in the collection
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	} 
+	
+	filter := bson.M{"_id": objectID}
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+	
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"message": "Todo deleted"})
 }
